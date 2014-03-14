4f862ddb96d2edb8a3d9af71d5e6c2b612cc0a69
alkarinv <alkarin.v@gmail.com>
2014-03-13 20:35:29 -0500

    Added new VerticalSpacing enum

--- src/mc/alk/util/objects/Hologram.java
+++ src/mc/alk/util/objects/Hologram.java
@@ -1,40 +1,53 @@
 package mc.alk.util.objects;
 
+import org.bukkit.Location;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import org.bukkit.Location;
-
 public class Hologram
 {
-	public static final double SPACED_TXT = 0.25;
-	public static final double COMPACT_TEXT = 0.23;
-	private double distanceBetweenLines;
-	private ArrayList<String> lines;
-	private Location location;
+    public enum VerticalTextSpacing {
+        COMPACT(0.23), SPACED(0.25);
+        final double spacing;
+        VerticalTextSpacing(double spacing) {
+            this.spacing = spacing;
+        }
 
-	public Hologram(String leaderboardName, double distanceBetweenLines,
-			Location location, String... lines)
-	{
-		this.lines = new ArrayList<String>();
-		this.distanceBetweenLines = distanceBetweenLines;
-		this.lines.set(0, leaderboardName);
-		this.lines.addAll(Arrays.asList(lines));
-		this.location = location;
-	}
+        public double spacing() {
+            return spacing;
+        }
+    }
+    private double distanceBetweenLines;
+    private ArrayList<String> lines;
+    private Location location;
 
-	public double getDistanceBetweenLines()
-	{
-		return distanceBetweenLines;
-	}
+    public Hologram(String leaderboardName, VerticalTextSpacing spacing,
+                    Location location, String... lines){
+        this(leaderboardName, spacing.spacing(), location, lines);
+    }
 
-	public ArrayList<String> getLines()
-	{
-		return lines;
-	}
+    public Hologram(String leaderboardName, double distanceBetweenLines,
+                    Location location, String... lines){
+        this.lines = new ArrayList<String>();
+        this.distanceBetweenLines = distanceBetweenLines;
+        this.lines.set(0, leaderboardName);
+        this.lines.addAll(Arrays.asList(lines));
+        this.location = location;
+    }
 
-	public Location getLocation()
-	{
-		return location;
-	}
+    public double getDistanceBetweenLines()
+    {
+        return distanceBetweenLines;
+    }
+
+    public ArrayList<String> getLines()
+    {
+        return lines;
+    }
+
+    public Location getLocation()
+    {
+        return location;
+    }
 }
