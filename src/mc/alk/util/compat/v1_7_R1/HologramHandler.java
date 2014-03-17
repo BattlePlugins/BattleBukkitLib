5db745c04da3c73c31d783d5e4206f2e9fc1c3d1
MCForger <mrseanmcd@gmail.com>
2014-03-16 20:33:26 -0400

    F - Added more color support

--- src/mc/alk/util/compat/v1_7_R1/HologramHandler.java
+++ src/mc/alk/util/compat/v1_7_R1/HologramHandler.java
@@ -14,6 +14,7 @@ import net.minecraft.server.v1_7_R1.PacketPlayOutSpawnEntityLiving;
 import net.minecraft.server.v1_7_R1.WorldServer;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_7_R1.CraftWorld;
 import org.bukkit.craftbukkit.v1_7_R1.entity.CraftPlayer;
@@ -51,7 +52,7 @@ public class HologramHandler implements IHologramHandler
 		horse.setLocation(location.getX(), location.getY() + 55,
 				location.getZ(), 0, 0);
 		horse.setAge(-1700000);
-		horse.setCustomName(text);
+		horse.setCustomName(ChatColor.translateAlternateColorCodes('&', text));
 		horse.setCustomNameVisible(true);
 		PacketPlayOutSpawnEntityLiving packedt = new PacketPlayOutSpawnEntityLiving(
 				horse);
