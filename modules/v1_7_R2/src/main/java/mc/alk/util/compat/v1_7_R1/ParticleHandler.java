dd851c32c8886dbf6e2dc3d0c54963d24a62e91d
Europia79 <nikolai.kalashnikov@hotmail.com>
2015-06-12 17:40:04 -0500

    Updated to 1.7.5

---
+++ modules/v1_7_R2/src/main/java/mc/alk/util/compat/v1_7_R1/ParticleHandler.java
@@ -0,0 +1,33 @@
+package mc.alk.util.compat.v1_7_R2;
+
+import mc.alk.util.handlers.IParticleHandler;
+import net.minecraft.server.v1_7_R2.PacketPlayOutWorldParticles;
+
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_7_R2.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
+
+/**
+ * For craftbukkit 1.7.5-R0.1
+ */
+public class ParticleHandler implements IParticleHandler {
+
+    @Override
+    public void sendEffect(Player player, ParticleEffects effectType,
+            Location location, Vector offSet, int speed, int count) {
+        try {
+            PacketPlayOutWorldParticles packet = new PacketPlayOutWorldParticles(
+                    effectType.getParticleName(), (float) location.getX(),
+                    (float) location.getY(), (float) location.getZ(),
+                    offSet.getBlockX(), offSet.getBlockY(), offSet.getBlockZ(),
+                    speed, count);
+
+            ((CraftPlayer) player).getHandle().playerConnection
+                    .sendPacket(packet);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
