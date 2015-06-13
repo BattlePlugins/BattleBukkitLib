bc96daa55bd4553b6da8d6d0c52bfeec3bb81dac
Europia79 <nikolai.kalashnikov@hotmail.com>
2015-06-13 01:50:29 -0500

    Deprecated & changed enum ParticleEffects
    I'm not sure if this class is even used. But as a precaution I added
    deprecations to several enumerations.
    
    Instead, enum ParticleEffects has been changed to mirror
    net.minecraft.server.EnumParticle.class
    
    https://github.com/Techcable/mc-dev/blob/1.8.4/net/minecraft/server/EnumParticle.java#L10
    
    Because, ultimately, it's going to be used for the construction of
    PacketPlayOutWorldParticles... which powers ParticleHandler::sendEffect()

--- modules/api/src/main/java/mc/alk/util/handlers/IParticleHandler.java
+++ modules/api/src/main/java/mc/alk/util/handlers/IParticleHandler.java
@@ -7,42 +7,84 @@ import org.bukkit.util.Vector;
 public interface IParticleHandler {
 
     public enum ParticleEffects {
-
-        HUGE_EXPLOSION("hugeexplosion"), 
-        LARGE_EXPLODE("largeexplode"), 
-        FIREWORKS_SPARK("fireworksSpark"), 
-        BUBBLE("bubble"), 
-        SUSPEND("suspend"), 
-        DEPTH_SUSPEND("depthSuspend"), 
-        TOWN_AURA("townaura"), 
-        CRIT("crit"), 
-        MAGIC_CRIT("magicCrit"), 
-        MOB_SPELL("mobSpell"), 
-        MOB_SPELL_AMBIENT("mobSpellAmbient"), 
-        SPELL("spell"), 
-        INSTANT_SPELL("instantSpell"), 
-        WITCH_MAGIC("witchMagic"), 
-        NOTE("note"), 
-        PORTAL("portal"), 
-        ENCHANTMENT_TABLE("enchantmenttable"), 
-        EXPLODE("explode"), 
-        FLAME("flame"), 
-        LAVA("lava"), 
-        FOOTSTEP("footstep"), 
-        SPLASH("splash"), 
-        LARGE_SMOKE("largesmoke"), 
-        CLOUD("cloud"), 
-        RED_DUST("reddust"), 
-        SNOWBALL_POOF("snowballpoof"), 
-        DRIP_WATER("dripWater"), 
-        DRIP_LAVA("dripLava"), 
-        SNOW_SHOVEL("snowshovel"), 
-        SLIME("slime"), 
-        HEART("heart"), 
-        ANGRY_VILLAGER("angryVillager"), 
-        HAPPY_VILLAGER("happyVillager"), 
-        ICONCRACK("iconcrack_"), 
-        TILECRACK("tilecrack_");
+        
+        EXPLOSION_NORMAL("explode"),
+        EXPLOSION_LARGE("largeexplode"),
+        EXPLOSION_HUGE("hugeexplosion"),
+        FIREWORKS_SPARK("fireworksSpark"),
+        WATER_BUBBLE("bubble"),
+        WATER_SPLASH("splash"),
+        WATER_WAKE("wake"), // no 1.7 equivalent
+        SUSPENDED("suspended"),
+        SUSPENDED_DEPTH("depthsuspend"),
+        CRIT("crit"),
+        CRIT_MAGIC("magicCrit"),
+        SMOKE_NORMAL("smoke"),
+        SMOKE_LARGE("largesmoke"),
+        SPELL("spell"),
+        SPELL_INSTANT("instantSpell"),
+        SPELL_MOB("mobSpell"),
+        SPELL_MOB_AMBIENT("mobSpellAmbient"),
+        SPELL_WITCH("witchMagic"),
+        DRIP_WATER("dripWater"),
+        DRIP_LAVA("dripLava"),
+        VILLAGER_ANGRY("angryVillager"),
+        VILLAGER_HAPPY("happyVillager"),
+        TOWN_AURA("townaura"),
+        NOTE("note"),
+        PORTAL("portal"),
+        ENCHANTMENT_TABLE("enchantmenttable"),
+        FLAME("flame"),
+        LAVA("lava"),
+        FOOTSTEP("footstep"),
+        CLOUD("cloud"),
+        REDSTONE("reddust"),
+        SNOWBALL("snowballpoof"),
+        SNOW_SHOVEL("snowshovel"),
+        SLIME("slime"),
+        HEART("heart"),
+        BARRIER("barrier"), // no 1.7 equivalent
+        ITEM_CRACK("iconcrack_"),
+        BLOCK_CRACK("blockcrack_"),
+        BLOCK_DUST("blockdust"),
+        WATER_DROP("droplet"), // no 1.7 equivalent
+        ITEM_TAKE("take"), // no 1.7 equivalent
+        MOB_APPEARANCE("mobappearance"), // no 1.7 equivalent
+        
+        /** @Deprecated in favor of EXPLOSION_NORMAL */
+        @Deprecated EXPLODE("explode"),
+        /** @Deprecated in favor of EXPLOSION_HUGE */
+        @Deprecated HUGE_EXPLOSION("hugeexplosion"), 
+        /** @Deprecated in favor of EXPLOSION_LARGE */
+        @Deprecated LARGE_EXPLODE("largeexplode"), 
+        /** @Deprecated in favor of WATER_BUBBLE */
+        @Deprecated BUBBLE("bubble"), 
+        /** @Deprecated in favor of SUSPENDED */
+        @Deprecated DEPTH_SUSPEND("depthsuspend"), 
+        /** @Deprecated in favor of CRIT_MAGIC */
+        @Deprecated MAGIC_CRIT("magicCrit"), 
+        /** @Deprecated in favor of SPELL_MOB */
+        @Deprecated MOB_SPELL("mobSpell"), 
+        /** @Deprecated in favor of SPELL_MOB_AMBIENT */
+        @Deprecated MOB_SPELL_AMBIENT("mobSpellAmbient"), 
+        /** @Deprecated in favor of SPELL_INSTANT */
+        @Deprecated INSTANT_SPELL("instantSpell"), 
+        /** @Deprecated in favor of SPELL_WITCH */
+        @Deprecated WITCH_MAGIC("witchMagic"), 
+        /** @Deprecated in favor of WATER_SPLASH */
+        @Deprecated SPLASH("splash"), 
+        /** @Deprecated in favor of SMOKE_LARGE */
+        @Deprecated LARGE_SMOKE("largesmoke"), 
+        /** @Deprecated in favor of REDSTONE */
+        @Deprecated RED_DUST("reddust"), 
+        /** @Deprecated in favor of SNOWBALL */
+        @Deprecated SNOWBALL_POOF("snowballpoof"), 
+        /** @Deprecated in favor of VILLAGER_ANGRY */
+        @Deprecated ANGRY_VILLAGER("angryVillager"), 
+        /** @Deprecated in favor of VILLAGER_HAPPY */
+        @Deprecated HAPPY_VILLAGER("happyVillager"), 
+        /** @Deprecated in favor of ITEM_CRACK */
+        @Deprecated ICONCRACK("iconcrack_");
 
         private String particleName;
 
