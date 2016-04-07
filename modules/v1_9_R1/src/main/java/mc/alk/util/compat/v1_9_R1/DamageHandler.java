23f22aed2fa8b012bb447eda476e7fe5e87ea88b
Europia79 <nikolai.kalashnikov@hotmail.com>
2016-04-07 11:11:39 -0500

    BattleBukkitLib v2.4
    - updated for Minecraft 1.9.0

---
+++ modules/v1_9_R1/src/main/java/mc/alk/util/compat/v1_9_R1/DamageHandler.java
@@ -0,0 +1,19 @@
+package mc.alk.util.compat.v1_9_R1;
+
+import mc.alk.util.handlers.IDamageHandler;
+
+import net.minecraft.server.v1_9_R1.DamageSource;
+import net.minecraft.server.v1_9_R1.EntityPlayer;
+
+import org.bukkit.craftbukkit.v1_9_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class DamageHandler implements IDamageHandler {
+
+    @Override
+    public void damageEntity(Player player, double dmg) {
+        EntityPlayer entity = ((CraftPlayer)player).getHandle();
+        entity.damageEntity(DamageSource.GENERIC, (float) dmg);
+    }
+
+}
