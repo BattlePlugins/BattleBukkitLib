5892ba473a18cff6b3bead35923f9f79aa9cab59
Albert Tang <tangalbert919@gmail.com>
2017-07-08 00:07:03 -0500

    Finish dependencies, add v1_9_R2 package

---
+++ modules/v1_9_R2/src/main/java/mc/alk/util/compat/v1_9_R2/DamageHandler.java
@@ -0,0 +1,17 @@
+package main.java.mc.alk.util.compat.v1_9_R2;
+
+import mc.alk.util.handlers.IDamageHandler;
+import net.minecraft.server.v1_9_R2.DamageSource;
+import net.minecraft.server.v1_9_R2.EntityPlayer;
+import org.bukkit.craftbukkit.v1_9_R2.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class DamageHandler implements IDamageHandler {
+
+    @Override
+    public void damageEntity(Player player, double dmg) {
+        EntityPlayer entity = ((CraftPlayer)player).getHandle();
+        entity.damageEntity(DamageSource.GENERIC, (float) dmg);
+    }
+
+}
