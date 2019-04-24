86c84c0ca2af2fe825bda24f0f33be0c592d0316
RednedEpic <redned@retronixgames.x10host.com>
2019-04-23 19:43:45 -0500

    Add 1.14 module

---
+++ modules/v1_14_R1/src/main/java/mc/alk/battlebukkitlib/compat/v1_14_R1/SignHandler.java
@@ -0,0 +1,17 @@
+package mc.alk.battlebukkitlib.compat.v1_14_R1;
+
+import mc.alk.battlebukkitlib.handlers.ISignHandler;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
+
+/**
+ * For craftbukkit 1.14
+ */
+public class SignHandler implements ISignHandler {
+
+    @Override
+    public void sendLines(Player player, Sign sign, String[] lines) {
+        player.sendSignChange(sign.getLocation(), lines);
+    }
+
+}
