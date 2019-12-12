5e65734f7298072ef6dd6f1b3fba5b527b46ebc8
RednedEpic <redned235@gmail.com>
2019-12-11 19:38:24 -0600

    BattleBukkitLib v4.5.0 w/ 1.15 support

---
+++ modules/v1_15_R1/src/main/java/mc/alk/battlebukkitlib/compat/v1_15_R1/HologramHandler.java
@@ -0,0 +1,56 @@
+package mc.alk.battlebukkitlib.compat.v1_15_R1;
+
+import mc.alk.battlebukkitlib.handlers.IHologramHandler;
+import mc.alk.battlebukkitlib.objects.Hologram;
+import net.minecraft.server.v1_15_R1.ChatComponentText;
+import net.minecraft.server.v1_15_R1.EntityArmorStand;
+import net.minecraft.server.v1_15_R1.EntityPlayer;
+import net.minecraft.server.v1_15_R1.EntityTypes;
+import net.minecraft.server.v1_15_R1.PacketPlayOutEntityDestroy;
+import net.minecraft.server.v1_15_R1.PacketPlayOutSpawnEntityLiving;
+import net.minecraft.server.v1_15_R1.WorldServer;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_15_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_15_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+import java.util.Collections;
+import java.util.List;
+
+
+public class HologramHandler implements IHologramHandler {
+
+    @Override
+    public boolean destroyHologram(Hologram hologram) {
+        int[] ints = new int[hologram.getIds().size()];
+        for (int j = 0; j < ints.length; j++) {
+            ints[j] = hologram.getIds().get(j);
+        }
+        PacketPlayOutEntityDestroy packet = new PacketPlayOutEntityDestroy(ints);
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            ((CraftPlayer) player).getHandle().playerConnection
+                    .sendPacket(packet);
+        }
+        hologram.setShowing(false);
+        return true;
+    }
+
+    @Override
+    public List<Integer> showLine(Location location, String text) {
+        WorldServer world = ((CraftWorld) location.getWorld()).getHandle();
+        EntityArmorStand stand = new EntityArmorStand(EntityTypes.ARMOR_STAND, world);
+        stand.setLocation(location.getX(), location.getY(), location.getZ(), 0, 0);
+        stand.setCustomName(new ChatComponentText(ChatColor.translateAlternateColorCodes('&', text)));
+        stand.setCustomNameVisible(true);
+        PacketPlayOutSpawnEntityLiving packedt = new PacketPlayOutSpawnEntityLiving(stand);
+        for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+            EntityPlayer nmsPlayer = ((CraftPlayer) player).getHandle();
+            nmsPlayer.playerConnection.sendPacket(packedt);
+
+        }
+        return Collections.singletonList(stand.getId());
+    }
+
+}
---
+++ modules/v1_15_R1/src/main/java/mc/alk/battlebukkitlib/compat/v1_15_R1/InventoryHandler.java
@@ -0,0 +1,200 @@
+package mc.alk.battlebukkitlib.compat.v1_15_R1;
+
+import java.awt.Color;
+import java.util.List;
+
+import mc.alk.battlebukkitlib.handlers.IInventoryHandler;
+
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class InventoryHandler implements IInventoryHandler {
+
+    @Override
+    public void setColor(ItemStack itemStack, Color color) {
+        if (itemStack.getItemMeta() == null) {
+            return;
+        }
+
+        if (itemStack.getItemMeta() instanceof LeatherArmorMeta) {
+            org.bukkit.Color bukkitColor = getBukkitColor(color);
+            LeatherArmorMeta armorMeta = (LeatherArmorMeta) itemStack.getItemMeta();
+            armorMeta.setColor(bukkitColor);
+            itemStack.setItemMeta(armorMeta);
+        }
+
+        if (itemStack.getItemMeta() instanceof PotionMeta) {
+            org.bukkit.Color bukkitColor = getBukkitColor(color);
+            PotionMeta potionMeta = (PotionMeta) itemStack.getItemMeta();
+            potionMeta.setColor(bukkitColor);
+            itemStack.setItemMeta(potionMeta);
+        }
+    }
+
+    @Override
+    public Color getColor(ItemStack itemStack) {
+        if (itemStack.getItemMeta() == null) {
+            return null;
+        }
+
+        if (itemStack instanceof LeatherArmorMeta) {
+            LeatherArmorMeta armorMeta = (LeatherArmorMeta) itemStack.getItemMeta();
+            return new Color(armorMeta.getColor().getRed(), armorMeta.getColor().getGreen(), armorMeta.getColor().getBlue());
+        }
+
+        if (itemStack instanceof PotionMeta) {
+            PotionMeta potionMeta = (PotionMeta) itemStack.getItemMeta();
+            return new Color(potionMeta.getColor().getRed(), potionMeta.getColor().getGreen(), potionMeta.getColor().getBlue());
+        }
+
+        return null;
+    }
+
+    @Override
+    public List<PotionEffect> getCustomEffects(ItemStack itemStack) {
+        if (itemStack.getItemMeta() == null || !(itemStack.getItemMeta() instanceof PotionMeta)) {
+            return null;
+        }
+
+        PotionMeta potionMeta = (PotionMeta) itemStack.getItemMeta();
+        return potionMeta.getCustomEffects();
+    }
+
+    @Override
+    public void addCustomEffect(ItemStack itemStack, PotionEffect effect) {
+        if (itemStack.getItemMeta() == null || !(itemStack.getItemMeta() instanceof PotionMeta)) {
+            return;
+        }
+
+        PotionMeta potionMeta = (PotionMeta) itemStack.getItemMeta();
+        potionMeta.addCustomEffect(effect, true);
+        itemStack.setItemMeta(potionMeta);
+    }
+
+    @Override
+    public void removeCustomEffect(ItemStack itemStack, PotionEffectType effectType) {
+        if (itemStack.getItemMeta() == null || !(itemStack.getItemMeta() instanceof PotionMeta)) {
+            return;
+        }
+
+        PotionMeta potionMeta = (PotionMeta) itemStack.getItemMeta();
+        potionMeta.removeCustomEffect(effectType);
+        itemStack.setItemMeta(potionMeta);
+    }
+
+    @Override
+    public void setLore(ItemStack itemStack, List<String> lore) {
+        if (itemStack.getItemMeta() == null) {
+            return;
+        }
+
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.setLore(lore);
+        itemStack.setItemMeta(itemMeta);
+    }
+
+    @Override
+    public List<String> getLore(ItemStack itemStack) {
+        if (itemStack.getItemMeta() == null) {
+            return null;
+        }
+
+        return itemStack.getItemMeta().getLore();
+    }
+
+    @Override
+    public void setDisplayName(ItemStack itemStack, String displayName) {
+        if (itemStack.getItemMeta() == null) {
+            return;
+        }
+
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.setDisplayName(displayName);
+        itemStack.setItemMeta(itemMeta);
+    }
+
+    @Override
+    public String getDisplayName(ItemStack itemStack) {
+        if (itemStack.getItemMeta() == null) {
+            return null;
+        }
+
+        return itemStack.getItemMeta().getDisplayName();
+    }
+
+    @Override
+    public void setOwnerName(ItemStack itemStack, String ownerName) {
+        if (itemStack.getItemMeta() == null || !(itemStack.getItemMeta() instanceof SkullMeta)) {
+            return;
+        }
+
+        SkullMeta skullMeta = (SkullMeta) itemStack.getItemMeta();
+        skullMeta.setOwner(ownerName);
+        itemStack.setItemMeta(skullMeta);
+    }
+
+    @Override
+    public String getOwnerName(ItemStack itemStack) {
+        if (itemStack.getItemMeta() == null || !(itemStack.getItemMeta() instanceof SkullMeta)) {
+            return null;
+        }
+
+        SkullMeta skullMeta = (SkullMeta) itemStack.getItemMeta();
+        return skullMeta.getOwner();
+    }
+
+    @Override
+    public void setUnbreakable(ItemStack itemStack, boolean unbreakable) {
+        if (itemStack.getItemMeta() == null) {
+            return;
+        }
+
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.setUnbreakable(unbreakable);
+        itemStack.setItemMeta(itemMeta);
+    }
+
+    @Override
+    public boolean isUnbreakable(ItemStack itemStack) {
+        if (itemStack.getItemMeta() == null) {
+            return false;
+        }
+
+        return itemStack.getItemMeta().isUnbreakable();
+    }
+
+    @Override
+    public void setCustomModelData(ItemStack itemStack, int data) {
+        if (itemStack.getItemMeta() == null) {
+            return;
+        }
+
+        ItemMeta itemMeta = (ItemMeta) itemStack;
+        itemMeta.setCustomModelData(data);
+        itemStack.setItemMeta(itemMeta);
+    }
+
+    @Override
+    public int getCustomModelData(ItemStack itemStack) {
+        if (itemStack.getItemMeta() == null || !(itemStack.getItemMeta().hasCustomModelData())) {
+            return 0;
+        }
+
+        return itemStack.getItemMeta().getCustomModelData();
+    }
+
+    @Override
+    public boolean isEnderChest(InventoryType type) {
+        return type == InventoryType.ENDER_CHEST;
+    }
+
+    private org.bukkit.Color getBukkitColor(Color color){
+        return org.bukkit.Color.fromRGB(color.getRed(), color.getGreen(), color.getBlue());
+    }
+}
