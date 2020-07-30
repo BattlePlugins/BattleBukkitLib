b289f4dad0df608c5dffa7821ec1c2e56727342b
Zach443 <zbfiorito@gmail.com>
2020-07-30 00:39:10 -0500

    Added module for 1.16
    Also bumped version to 4.5.3

---
+++ modules/v1_16_R1/src/main/java/mc/alk/battlebukkitlib/compat/v1_16_R1/SignHandler.java
@@ -0,0 +1,29 @@
+package mc.alk.battlebukkitlib.compat.v1_16_R1;
+
+import mc.alk.battlebukkitlib.handlers.ISignHandler;
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.Directional;
+import org.bukkit.entity.Player;
+
+/**
+ * For craftbukkit 1.15
+ */
+public class SignHandler implements ISignHandler {
+
+    @Override
+    public void sendLines(Player player, Sign sign, String[] lines) {
+        player.sendSignChange(sign.getLocation(), lines);
+    }
+
+    @Override
+    public Block getAttachedBlock(Sign sign) {
+        BlockData blockData = sign.getBlockData();
+        if (!(blockData instanceof Directional))
+            return null;
+
+        Directional directional = (Directional) blockData;
+        return sign.getBlock().getRelative(directional.getFacing().getOppositeFace());
+    }
+}
